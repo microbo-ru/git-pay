835c854932da4a8cd0be16701804c0ea2c673655
ivanblinov2k17 <ivanblinov2015@mail.ru>
2020-04-23 21:04:17 +0300

    added login button with overlay

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar app color="indigo" dark>
+    <v-toolbar-title>Application</v-toolbar-title>
+    <v-spacer></v-spacer>
+
+    <v-icon size="x-large" @click="showOverlay">mdi-account-box</v-icon>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data: () => ({}),
+  methods: {
+    showOverlay() {
+      this.$emit("show-overlay");
+    }
+  }
+};
+</script>
---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container width="100%" height="100%">
+    <Header @show-overlay="showOverlay"></Header>
+    <v-overlay :value="overlay" absolute="true">
+      <v-btn icon @click="overlay = false">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <Login />
+    </v-overlay>
+  </v-container>
+</template>
+
+<script>
+import Header from "./Header";
+import Login from "./Login";
+export default {
+  name: "Main",
+  components: {
+    Header,
+    Login
+  },
+
+  data: () => ({
+    overlay: null
+  }),
+  methods: {
+    showOverlay() {
+      this.overlay = !this.overlay;
+    }
+  }
+};
+</script>
