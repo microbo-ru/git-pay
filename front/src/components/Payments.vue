20c5b8354f04eedf0066e6ff4181b4d4304f1909
Yuriy Rogachev <y_rogachev@inbox.ru>
2020-04-25 04:40:25 +0400

    moved payments to own component

---
+++ front/src/components/Payments.vue
@@ -0,0 +1,71 @@
+<template>
+  <div style="width: 100%; text-align: center">
+    <form
+      id="testForm"
+      method="POST"
+      action="https://api.dev.walletone.com/p2p/v2/payer"
+      @submit="handleForm"
+    >
+      <input
+        type="hidden"
+        name="PhoneNumber"
+        id="PhoneNumber"
+        value="+79818925700"
+      />
+      <input
+        type="hidden"
+        name="PlatformPayerId"
+        id="PlatformPayerId"
+        value="microbo"
+      />
+      <input type="hidden" name="PlatformId" id="PlatformId" value="microbo" />
+      <input
+        type="hidden"
+        name="ReturnUrl"
+        id="ReturnUrl"
+        value="http://microbo.ru/w1"
+      />
+      <input type="hidden" name="Timestamp" id="Timestamp" />
+      <input type="hidden" name="Signature" id="Signature" value="" />
+
+      <input type="submit" value="submit" style="color: blue;" />
+    </form>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  name: "Payments",
+  methods: {
+    handleForm() {
+      var key = "TZvds4HWduo6unK9emOFzIuRHXMueUXUREk15tSDdJ2yY0x7TJuDUD";
+
+      var date = moment()
+        .utc()
+        .format("YYYY-MM-DDTHH:mm:ss");
+      document.getElementById("Timestamp").value = date;
+      //Signature, который формируется путем объединения значений всех остальных параметров формы (с учетом параметра Timestamp) в алфавитном порядке их имен (без учета регистра)
+      // с добавлением в конец «секретного ключа» площадки. Полученное после объединения параметров и «секретного ключа» значение, представленное в кодировке UTF-8,
+      // хешируется выбранным методом формирования ЭЦП и его байтовое представление кодируется в Base64.
+      //Signature = Base64(SignatureMethod(UTF8(Sort(Params) + SignatureKey)))
+      var params =
+        document.getElementById("PhoneNumber").value +
+        document.getElementById("PlatformPayerId").value +
+        document.getElementById("PlatformId").value +
+        document.getElementById("ReturnUrl").value +
+        date;
+      var crypto = require("crypto");
+      var iconv = require("iconv-lite");
+      var encodedKey = crypto
+        .createHash("sha256")
+        .update(iconv.encode(params + key, "UTF-8"))
+        .digest("base64");
+      console.log("encodedKey:" + encodedKey);
+      document.getElementById("Signature").value = encodedKey;
+    },
+  },
+};
+</script>
+
+<style></style>
--- front/src/components/Projects/Projects.vue
+++ front/src/components/Projects/Projects.vue
@@ -9,85 +9,21 @@
       </v-row>
       <v-row> <ContractsList /></v-row>
     </v-container>
-    <div style="width: 100%; text-align: center">
-      <form
-        id="testForm"
-        method="POST"
-        action="https://api.dev.walletone.com/p2p/v2/payer"
-        @submit="handleForm"
-      >
-        <input
-          type="hidden"
-          name="PhoneNumber"
-          id="PhoneNumber"
-          value="+79818925700"
-        />
-        <input
-          type="hidden"
-          name="PlatformPayerId"
-          id="PlatformPayerId"
-          value="microbo"
-        />
-        <input
-          type="hidden"
-          name="PlatformId"
-          id="PlatformId"
-          value="microbo"
-        />
-        <input
-          type="hidden"
-          name="ReturnUrl"
-          id="ReturnUrl"
-          value="http://microbo.ru/w1"
-        />
-        <input type="hidden" name="Timestamp" id="Timestamp" />
-        <input type="hidden" name="Signature" id="Signature" value="" />
-
-        <input type="submit" value="submit" style="color: blue;" />
-      </form>
-    </div>
+    <Payments />
   </div>
 </template>
 
 <script>
 import ContractsList from "./ContractsList";
 import TypeSelector from "./TypeSelector";
-
-import moment from "moment";
+import Payments from "../Payments";
 
 export default {
   name: "Projects",
   components: {
     ContractsList,
     TypeSelector,
-  },
-  methods: {
-    handleForm() {
-      var key = "TZvds4HWduo6unK9emOFzIuRHXMueUXUREk15tSDdJ2yY0x7TJuDUD";
-
-      var date = moment()
-        .utc()
-        .format("YYYY-MM-DDTHH:mm:ss");
-      document.getElementById("Timestamp").value = date;
-      //Signature, который формируется путем объединения значений всех остальных параметров формы (с учетом параметра Timestamp) в алфавитном порядке их имен (без учета регистра)
-      // с добавлением в конец «секретного ключа» площадки. Полученное после объединения параметров и «секретного ключа» значение, представленное в кодировке UTF-8,
-      // хешируется выбранным методом формирования ЭЦП и его байтовое представление кодируется в Base64.
-      //Signature = Base64(SignatureMethod(UTF8(Sort(Params) + SignatureKey)))
-      var params =
-        document.getElementById("PhoneNumber").value +
-        document.getElementById("PlatformPayerId").value +
-        document.getElementById("PlatformId").value +
-        document.getElementById("ReturnUrl").value +
-        date;
-      var crypto = require("crypto");
-      var iconv = require("iconv-lite");
-      var encodedKey = crypto
-        .createHash("sha256")
-        .update(iconv.encode(params + key, "UTF-8"))
-        .digest("base64");
-      console.log("encodedKey:" + encodedKey);
-      document.getElementById("Signature").value = encodedKey;
-    },
+    Payments,
   },
 };
 </script>
