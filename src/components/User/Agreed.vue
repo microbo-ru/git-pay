4b95ab923960e12620952fa468223a584301403d
Yuriy Rogachev <y_rogachev@inbox.ru>
2020-04-26 01:06:40 +0400

    test

--- src/components/User/Agreed.vue
+++ src/components/User/Agreed.vue
@@ -1,12 +1,20 @@
 <template>
-  <div v-if="$store.getters.get_agreed.empl_username == $store.state.username">
-    <div>{{$store.getter.get_agreed.user_username }}</div>
+  <div>
+    <div v-if="$store.getters.get_agreed.empl_username == $store.state.username">
+      <div>{{$store.getter.get_agreed.user_username }}</div>
+    </div>
+    <v-btn @click="showState">showstate</v-btn>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Agreed"
+  name: "Agreed",
+  methods: {
+    showState() {
+      console.log(this.$store.state);
+    }
+  }
 };
 </script>
 
